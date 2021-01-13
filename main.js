68f27a93e08f4e316c5cb29a5f21eaaff8ce3bb0
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-13 16:54:15 +0100

    Slider scorrevole con tastiera

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-$('.next').click(function() {
+// Passare alla slide precedente
+function prevSlide() {
+  if (!$('img.active').hasClass('first')) {
+    $('.active').removeClass('active').prev().addClass('active');
+  }
+}
+
+// Passare alla slide successiva
+function nextSlide() {
   if (!$('img.active').hasClass('last')) {
     $('.active').removeClass('active').next().addClass('active');
-    }
   }
-);
+}
 
-$('.prev').click(function() {
-    if (!$('img.active').hasClass('first')) {
-      $('.active').removeClass('active').prev().addClass('active');
+// Cambiare slide via click
+$('.prev').click(prevSlide);
+$('.next').click(nextSlide);
+
+// Cambiare slide via tastiera
+$(document).keydown(function (key) {
+  switch (key.keyCode) {
+    case 37:
+      prevSlide();
+    break;
+    case 39:
+      nextSlide();
+    break;
     }
   }
 );
